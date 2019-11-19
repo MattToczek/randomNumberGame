fdb99931b9bee4706ba87122b84ecdcfcf88b09f
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-19 11:13:27 +0000

    added functionality to compare random number with user generated - turn counted not working yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import Application from './Button/Button';
 import RandomNum from './randomNum';
+import Application from './Button/Button';
+
 import Checker from './Checker'
 
 function App() {
+
+const[counter, setCounter] = useState(0);
+const[rNumb, setRnumb] = useState(1);
+const[turns, setTurns] = useState(0);
+
+
+const getRandNumber = () => {
+  let a = Math.ceil(Math.random()*100);
+  setRnumb(a);
+}
+
+   
   return (
     <div className="App">
-      <RandomNum />
-      <Application />
+      <a href="#" onClick={getRandNumber}>Get Rand Number</a>
+      <h1>{rNumb ? `Can you make ${rNumb}` : "Please click the button"}</h1>
+      
+      <Application setTurns={setTurns} turns= {turns} counter={counter} setCounter={setCounter}/>
+      <p>My randstate number is {rNumb}</p>
+      <h2>{( counter == rNumb) ? `You win, it took you ${turns} turns!` : "Try again"}</h2>
     </div>
   );
 
--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import ReactDOM from 'react-dom';
 import Display from './Display'
 // import RandomNum from '../randomNum';
@@ -23,24 +23,27 @@ function Button(props) {
 
 //App
 
-function Application() {
+function Application(props) {
     // let randomNo = RandomNum();
-    const[counter, setCounter] = useState(0);
-    const plusOne = ()=> setCounter(counter+1);
-    const plusThree = ()=> setCounter(counter+3);
-    const timesTwo = ()=> setCounter(counter*2);
-    const toZero = ()=> setCounter(counter-counter);
+    
+    const plusOne = ()=> props.setCounter(props.counter+1);
+    const plusThree = ()=> props.setCounter(props.counter+3);
+    const timesTwo = ()=> props.setCounter(props.counter*2);
+    const toZero = ()=> props.setCounter(props.counter-props.counter);
+    const addTurns = ()=> props.setTurns(props.turns+1);
+    console.log(props.turns);
+    
     return (
         
         <div>
            
-            <Button label="+1" onClickFunc = {plusOne}/>
-            <Button label="+3" onClickFunc = {plusThree}/>
-            <Button label="x2" onClickFunc = {timesTwo}/>
-            <Button label="Clear" onClickFunc = {toZero}/>
+            <Button label="+1" onClickFunc = {plusOne, addTurns}/>
+            <Button label="+3" onClickFunc = {plusThree, addTurns}/>
+            <Button label="x2" onClickFunc = {timesTwo, addTurns}/>
+            <Button label="Clear" onClickFunc = {toZero, addTurns}/>
 
 
-            <Display message = {counter}/>
+            <Display message = {props.counter}/>
         </div>
     );
 }
@@ -50,5 +53,50 @@ function Application() {
 
 //     document.getElementById('root'),
 // );
+
+
+//App
+
+// class Application extends React.Component {
+//     // let randomNo = RandomNum();
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {counter: 0 };
+//     // }
+
+//     state = {
+//         counter: 0
+//     }
+
+//     // myFunc = () => {
+
+//     // }
+
+
+//     render(){
+//         // const[counter, setCounter] = useState(0);
+
+
+//         const plusOne = ()=> setCounter(counter+1);
+//         const plusThree = ()=> setCounter(counter+3);
+//         const timesTwo = ()=> setCounter(counter*2);
+//         const toZero = ()=> setCounter(counter-counter);
+//         // if (counter == )
+        
+//         return (
+            
+//             <div>
+            
+//                 <Button label="+1" onClickFunc = {plusOne}/>
+//                 <Button label="+3" onClickFunc = {plusThree}/>
+//                 <Button label="x2" onClickFunc = {timesTwo}/>
+//                 <Button label="Clear" onClickFunc = {toZero}/>
+
+
+//                 <Display message = {counter}/>
+//             </div>
+//         );
+//     }
+// }
 
 export default Application;
